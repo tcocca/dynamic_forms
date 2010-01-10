3cde0af5819924943ac6d18bea00150e4972fe74
tcocca <tom.cocca@gmail.com>
2010-01-09 21:16:20 -0500

    updated the generator to generate the models

--- generators/dynamic_forms/dynamic_forms_generator.rb
+++ generators/dynamic_forms/dynamic_forms_generator.rb
@@ -2,6 +2,19 @@ class DynamicFormsGenerator < Rails::Generator::Base
   
   def manifest
     record do |m|
+      m.directory "app/models"
+      m.template "models/form.rb", "app/models/form.rb"
+      m.template "models/form_field.rb", "app/models/form_field.rb"
+      m.template "models/form_field_option.rb", "app/models/form_field_option.rb"
+      m.template "models/form_submission.rb", "app/models/form_submission.rb"
+      
+      m.directory "app/models/form_field"
+      m.template "models/form_field/check_box.rb", "app/models/form_field/check_box.rb"
+      m.template "models/form_field/check_box_group.rb", "app/models/form_field/check_box_group.rb"
+      m.template "models/form_field/select.rb", "app/models/form_field/select.rb"
+      m.template "models/form_field/text_area.rb", "app/models/form_field/text_area.rb"
+      m.template "models/form_field/text_field.rb", "app/models/form_field/text_field.rb"
+      
       m.migration_template 'migrations/create_dynamic_forms.rb', 'db/migrate', :migration_file_name => "create_dynamic_forms"
     end
   end
